70031695932711ba9680d7c5047027ea564e5e81
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-07-19 06:01:01 +0300

    initial commit

---
+++ razv4_0/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Razv40Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'razv4_0'
---
+++ razv4_0/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    contact = models.CharField(max_length=255)
+    mappoint = models.CharField(max_length=255, default='', help_text="Yandex mappoint")
+    subcontractor = models.BooleanField(default=False, help_text='True if subcontractor')
+
+    def __repr__(self):
+        return f"Customer(name={self.name!r}, contact={self.contact!r})"
+
+    def __str__(self):
+        return self.name
+
+
+class Razvozka(models.Model):
+    date = models.DateField(help_text='date of transportation')
+    date_id = models.SmallIntegerField(default=0, help_text='order inside date')
+    customer = models.ForeignKey(Customer, models.SET_NULL, null=True, blank=True, help_text='customer base if exist')
+    customer_name = models.CharField(max_length=100, help_text='customer as text could differ from db')
+    address = models.CharField(max_length=255, help_text='real address')
+    contact = models.CharField(max_length=255, help_text='real contacts')
+    to_do_deliver = models.CharField(max_length=255, help_text='things for delivery')
+    to_do_take = models.CharField(max_length=255, help_text='things to take from')
+    map_point = models.CharField(max_length=255, help_text='Yandex mappoint')
+    fulfilled = models.BooleanField(default=True, help_text='True is fulfilled')
+    deliver_to = models.BooleanField(default=False, help_text='transportation to processing')
+    return_from = models.BooleanField(default=False, help_text='return products from processing')
+    return_all = models.BooleanField(default=False, help_text='False if some part was not return')
+    return_goods = models.ForeignKey('self', models.SET_NULL, null=True, blank=True,
+                                     help_text='from which delivery return')
+    date_until = models.DateField(default='2022-08-01', null=True, blank=True, help_text='plan (last) date of transportation')
+    date_create = models.DateField(default='2022-08-01', null=True, blank=True, help_text='date of create razvozka')
+
+    def __str__(self):
+        return str(self.date) + '| ' + str(self.customer_name) + '| ' + str(self.to_do_deliver)+ '| ' + str(self.to_do_take)
+
+    def __repr__(self):
+        return f"razvozka_list(date={self.date!r}, customer={self.customer_name!r}, deliver={self.to_do_deliver!r}, " \
+               f"take={self.to_do_take!r}) "
+
+    def get_all_todo(self):
+        to_do_take = ''
+        to_do_deliver = ''
+        if self.to_do_take != '':
+            to_do_take = ' ЗАБРАТЬ: ' + str(self.to_do_take)
+        if self.to_do_deliver != '':
+            to_do_deliver = ' СДАТЬ: ' + str(self.to_do_deliver)
+        return f"{to_do_take} {to_do_deliver}"
