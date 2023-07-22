f5983c6645d2577d5e72edadfdca1fe96e06fa59
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-07-22 15:50:46 +0300

    initial commit

---
+++ razv4_0/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+app_name = 'razv4_0'
+
+urlpatterns = [
+    path('accounts/login/', views.custom_login, name='custom_login'),
+    path('accounts/logout/', views.custom_logout, name='custom_logout'),
+
+    path('current/', views.current_rzv, name='current_rzv'),
+    path('razvozka_delete/<int:razv_id>', views.razvozka_delete, name='razvozka_delete'),
+    path('razvozka_fulfilled/<int:razv_id>', views.razvozka_fulfilled, name='razvozka_fulfilled'),
+    path('razvozka_unfulfilled/<int:razv_id>', views.razvozka_unfulfilled, name='razvozka_unfulfilled'),
+    path('razvozka_returned_all/<int:razv_id>', views.razvozka_returned_all, name='razvozka_returned_all'),
+    path('razvozka_deliver_to/<int:razv_id>', views.razvozka_returned_all, name='razvozka_deliver_to'),
+
+]
+
