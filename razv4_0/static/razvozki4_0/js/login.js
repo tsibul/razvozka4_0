dfd8a6dd1a3713cab071e803ab5cf9234b424f40
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-07-22 15:50:23 +0300

    modal code for authorisation

---
+++ razv4_0/static/razvozki4_0/js/login.js
@@ -0,0 +1,34 @@
+// logout_modal.js
+const authModal = document.getElementById('authModal');
+const closeButtons = document.querySelectorAll('.close');
+const loginForm = document.getElementById('login-form');
+const logoutForm = document.getElementById('logout-form');
+
+function openModal() {
+    authModal.style.display = 'block';
+}
+
+function closeModal() {
+    authModal.style.display = 'none';
+}
+
+closeButtons.forEach(function (row) {
+    row.addEventListener("click", () => {
+        // Get the data from the clicked row
+        closeModal()
+    });
+});
+
+// При клике на кнопку "вход" отображаем форму входа и скрываем форму выхода
+    document.getElementById('login-button').addEventListener('click', function () {
+        loginForm.style.display = 'block';
+        logoutForm.style.display = 'none';
+        openModal();
+    });
+
+// При клике на кнопку "выход" отображаем форму выхода и скрываем форму входа
+    document.getElementById('logout-button').addEventListener('click', function () {
+        logoutForm.style.display = 'block';
+        loginForm.style.display = 'none';
+        openModal();
+    });
