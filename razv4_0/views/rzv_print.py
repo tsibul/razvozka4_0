3b8fbaa5311ec7535f1f5d5d36ba4c5335db723c
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-07-25 00:37:33 +0300

    changed params not to interfere with global names added the Queries

--- razv4_0/views/rzv_print.py
+++ razv4_0/views/rzv_print.py
@@ -1,11 +1,15 @@
 from razv4_0.models import Razvozka
 from django.shortcuts import render
+from datetime import datetime, date
 
 
-def print_all(request):
-    context = {}
+def print_all(request, date_rzv):
+    razvozki = Razvozka.objects.filter(date=datetime.strptime(date_rzv, '%Y-%m-%d').date())
+    context = {'razvozki': razvozki}
     return render(request, 'print.html', context)
 
-def print_one(request, id):
-    context = {}
+
+def print_one(request, date_rzv, driver_id):
+    razvozki = Razvozka.objects.filter(date=datetime.strptime(date_rzv, '%Y-%m-%d').date(), driver__id=driver_id)
+    context = {'razvozki', razvozki}
     return render(request, 'print.html', context)
