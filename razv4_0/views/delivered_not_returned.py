a9bc6f98c7609d806b942b747c4fae4049ca8926
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-07-29 14:29:05 +0300

    Index shows count of unreturned

--- razv4_0/views/delivered_not_returned.py
+++ razv4_0/views/delivered_not_returned.py
@@ -5,9 +5,11 @@ from razv4_0.models import Driver, Customer, Razvozka
 
 def deliver_no_return(request):
     navi = 'deliver'
-    razvozki = Razvozka.objects.filter(date__isnull=False, return_all=False, deliver_to=True,
-                                       customer__subcontractor=True).order_by('-date', 'date_id')[:50]
+
+    razvozki = Razvozka.objects.filter(date__isnull=False, return_all=False, deliver_to=True, fulfilled=True,
+                                       customer__subcontractor=True).order_by('-date', 'date_id')
+    to_return = razvozki.count()
     customers = Customer.objects.all().order_by('name')
     drivers = Driver.objects.all().order_by('id')
-    context = {'razvozki': razvozki, 'navi': navi, 'customers': customers, 'drivers': drivers}
-    return render(request, 'total_rzv.html', context)
+    context = {'razvozki': razvozki, 'navi': navi, 'customers': customers, 'drivers': drivers, 'to_return': to_return}
+    return render(request, 'delivered.html', context)
