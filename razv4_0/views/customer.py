b66a49fd19477906acf0752510ab72272243fcc3
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-07-31 08:09:22 +0300

    added to_return

--- razv4_0/views/customer.py
+++ razv4_0/views/customer.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from razv4_0.models import Customer
+from razv4_0.models import Customer, Razvozka
 
 
 def customer(request):
     navi = 'customer'
     customers = Customer.objects.all().order_by('name')[:49]
     end_customer = Customer.objects.all()[49:50]
-    context = {'navi': navi, 'customers': customers, 'end_customer': end_customer}
+    to_return = Razvozka.objects.filter(date__isnull=False, return_all=False, deliver_to=True,
+                                        fulfilled=True, customer__subcontractor=True).count()
+    context = {'navi': navi, 'customers': customers, 'end_customer': end_customer, 'to_return': to_return}
     return render(request, 'customer.html', context)
