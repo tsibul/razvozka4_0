51f0caa806043e02fcabe2a6545b7bd3ed68d7d5
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-08-03 08:06:25 +0300

    order icons by id

--- razv4_0/views/driver.py
+++ razv4_0/views/driver.py
@@ -10,7 +10,7 @@ from razv4_0.models import Driver, DriverIcons, Razvozka
 def driver(request):
     navi = 'diver'
     drivers = Driver.objects.filter(deleted=False).order_by('id')
-    driver_icons = DriverIcons.objects.all().order_by('description')
+    driver_icons = DriverIcons.objects.all().order_by('id')
     to_return = Razvozka.objects.filter(date__isnull=False, return_all=False, deliver_to=True,
                                         fulfilled=True, customer__subcontractor=True, deleted=False).count()
     context = {'navi': navi, 'to_return': to_return, 'drivers': drivers, 'driver_icons': driver_icons}
@@ -28,3 +28,9 @@ def driver_as_json(request, drv_id):
     drv = Driver.objects.get(id=drv_id).__dict__
     json_driver = json.dumps(drv, ensure_ascii=False, default=str)
     return JsonResponse(json_driver, safe=False)
+
+
+def driver_icon_as_json(request, drv_url):
+    drv_icon = DriverIcons.objects.get(icon_url=drv_url).__dict__
+    json_driver_icon = json.dumps(drv_icon, ensure_ascii=False, default=str)
+    return JsonResponse(json_driver_icon, safe=False)
