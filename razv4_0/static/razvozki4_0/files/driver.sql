385a675a7514cc774e6a8e78b3bebb6768a0503a
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-08-05 18:41:36 +0300

    import drivers

---
+++ razv4_0/static/razvozki4_0/files/driver.sql
@@ -0,0 +1,28 @@
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               11.1.0-MariaDB - mariadb.org binary distribution
+-- Операционная система:         Win64
+-- HeidiSQL Версия:              12.5.0.6677
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Дамп данных таблицы razv4_0.razv4_0_driver: ~4 rows (приблизительно)
+INSERT INTO `razv4_0_driver` (`id`, `code`, `description`, `phone1`, `phone2`, `car_no`, `icon_code`, `deleted`) VALUES
+	(1, 'ДТ', 'Дятел', '+79030027706', '+79266235938', 'х110те777', 'razvozki4_0/icons/kiwi.svg', 0),
+	(2, 'ИГ', 'Шеф', '+79039606519', '+79266026441', 'о203нт197', 'razvozki4_0/icons/earlybird.svg', 0),
+	(3, 'Н', 'Наемная Машина', '00', '', '', 'razvozki4_0/icons/cyrcle-question.svg', 0),
+	(4, 'РТ', 'Ренат', '+79030027706', '', 'х110те777', 'razvozki4_0/icons/bicycle.svg', 0);
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
