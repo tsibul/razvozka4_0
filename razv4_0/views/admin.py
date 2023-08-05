947d15f0502789af0d43188d993ecd236ca5eabb
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-08-05 18:41:30 +0300

    import drivers

--- razv4_0/views/admin.py
+++ razv4_0/views/admin.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.templatetags.static import static
 from django.urls import reverse
 
-from razv4_0.models import Razvozka
+from razv4_0.models import Razvozka, ImportRazvozka, ImportCustomer, Customer, Razvozka_returns
 
 
 def admin(request):
@@ -19,6 +19,7 @@ def admin(request):
 def admin_import(request):
     rzv_sql_path = 'razv4_0/static/razvozki4_0/files/razvozka.sql'
     cst_sql_path = 'razv4_0/static/razvozki4_0/files/customer.sql'
+    drv_sql_path = 'razv4_0/static/razvozki4_0/files/driver.sql'
     with open(cst_sql_path, 'r', encoding='utf-8') as cst_file:
         cst_queries = cst_file.read()
     cst_queries = cst_queries.replace('INSERT INTO `razvozki_customer`', 'INSERT INTO `razv4_0_importcustomer`')
@@ -29,4 +30,13 @@ def admin_import(request):
     rzv_queries = rzv_queries.replace('INSERT INTO `razvozki_razvozka`', 'INSERT INTO `razv4_0_importrazvozka`')
     with connection.cursor() as cursor:
         cursor.execute(rzv_queries)
+    with open(drv_sql_path, 'r', encoding='utf-8') as drv_file:
+        drv_queries = drv_file.read()
+    with connection.cursor() as cursor:
+        cursor.execute(drv_queries)
+
     return HttpResponseRedirect(reverse('razv4_0:admin'))
+
+
+def admin_delete_all():
+    return
