86a89da9b34f626777b64b5b433b556a8c1e738b
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-08-11 08:08:03 +0300

    for deploy

--- razvozki4_0/wsgi.py
+++ razvozki4_0/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'razvozki4_0.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'razvozki4_0.settings_prd')
 
 application = get_wsgi_application()
