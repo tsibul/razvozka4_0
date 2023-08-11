af71b69c3e3e657fbea5460725bd443ba4544485
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-08-11 23:05:49 +0300

    new imports

--- razv4_0/static/razvozki4_0/files/driver.sql
+++ razv4_0/static/razvozki4_0/files/driver.sql
@@ -15,11 +15,11 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 -- Дамп данных таблицы razv4_0.razv4_0_driver: ~4 rows (приблизительно)
-INSERT INTO `razv4_0_driver` (`id`, `code`, `description`, `phone1`, `phone2`, `car_no`, `icon_code`, `deleted`) VALUES
-	(1, 'ДТ', 'Дятел', '+79030027706', '+79266235938', 'х110те777', 'razvozki4_0/icons/kiwi.svg', 0),
-	(2, 'ИГ', 'Шеф', '+79039606519', '+79266026441', 'о203нт197', 'razvozki4_0/icons/earlybird.svg', 0),
-	(3, 'Н', 'Наемная Машина', '00', '', '', 'razvozki4_0/icons/cyrcle-question.svg', 0),
-	(4, 'РТ', 'Ренат', '+79030027706', '', 'х110те777', 'razvozki4_0/icons/bicycle.svg', 0);
+INSERT INTO `razv4_0_driver` (`id`, `code`, `description`, `phone1`, `phone2`, `car_no`, `icon_code`, `deleted`, `car_type`, `driver_name`, `car_model`) VALUES
+	(1, 'ДТ', 'Дятел', '+79030027706', '+79266235938', 'х110те777', 'razvozki4_0/icons/kiwi.svg', 0, 'грузовой В до 3.5 т', 'Алексеев Сергей Евгеньевич', 'Citroen Jumper'),
+	(2, 'ИГ', 'Шеф', '+79039606519', '+79266026441', 'о203нт197', 'razvozki4_0/icons/crown.svg', 0, 'легковой В', 'Цигельницкий Илья Генрихович', 'Subaru OutBack'),
+	(3, 'Н', 'Наемная Машина', '00', '', '00', 'razvozki4_0/icons/cyrcle-question.svg', 0, '00', '00', ''),
+	(4, 'РТ', 'Ренат', '+79030027706', '', 'х110те777', 'razvozki4_0/icons/bicycle.svg', 0, 'грузовой В до 3.5 т', 'Ренат', 'Citroen Jumper');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
