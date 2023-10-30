85452c3821da9a33363253dc98036060a6f908f0
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-10-30 08:10:38 +0300

    change admin url

--- razvozki4_0/urls.py
+++ razvozki4_0/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admi_rzv/', admin.site.urls),
     path('rzv/', include('razv4_0.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
