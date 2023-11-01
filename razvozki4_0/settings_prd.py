ad9cd4cb9ba23bdd7e6b86a96c5877fe15fbd9c7
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-11-02 02:51:58 +0300

    change static url

--- razvozki4_0/settings_prd.py
+++ razvozki4_0/settings_prd.py
@@ -100,7 +100,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/razv4_0'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Путь, куда будут собраны статические файлы
 
 
